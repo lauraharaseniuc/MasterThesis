77672e45d33cde054fd472911b069d0e4d6fe11c
laura.haraseniuc <laura.haraseniuc@co-era.com>
2026-01-01 22:58:08 +0200

    Initial commit
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/backend/computer_science_website/ComputerScienceWebsiteApplication.java
@@ -0,0 +1,14 @@
+package backend.computer_science_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputerScienceWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputerScienceWebsiteApplication.class, args);
+		System.out.println("Merge!");
+	}
+
+}
---
+++ src/test/java/backend/computer_science_website/ComputerScienceWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.computer_science_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputerScienceWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
