97bbb8edcfd210858e3bf374e5672a0e16a8f52e
laura.haraseniuc <laura.haraseniuc@co-era.com>
2026-03-30 16:02:14 +0300

    add dummy navbar

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  subMenuItems = [
+    'Item 1', 'Item 2', 'Item 3',
+    'Item 4', 'Item 5', 'Item 6',
+    'Item 7', 'Item 8', 'Item 9'
+  ];
 }
--- frontend/src/app/homepage/homepage.component.ts
+++ frontend/src/app/homepage/homepage.component.ts
@@ -1,12 +1,9 @@
 import {Component} from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [
-    MatIcon
-  ],
+  imports: [],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
