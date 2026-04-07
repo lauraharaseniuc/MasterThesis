c4d03cd1e6eae62751e896273da4575fa4add021
lauraharaseniuc <laura.haraseniuc@stud.ubbcluj.ro>
2026-04-07 17:25:34 +0300

    round12 vercel deploy

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,12 +4,14 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {provideAnimations} from "@angular/platform-browser/animations";
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
-    provideAnimations()
+    provideAnimations(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
   ]
 };
