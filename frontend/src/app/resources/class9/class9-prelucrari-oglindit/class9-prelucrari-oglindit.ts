b64656b3a02095eadcc53a55e226b93aaa4d05dc
lauraharaseniuc <laura.haraseniuc@stud.ubbcluj.ro>
2026-08-01 19:14:04 +0300

    add resources part 4

---
+++ frontend/src/app/resources/class9/class9-prelucrari-oglindit/class9-prelucrari-oglindit.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-class9-prelucrari-oglindit',
+  standalone: true,
+  imports: [],
+  templateUrl: './class9-prelucrari-oglindit.html',
+  styleUrl: './class9-prelucrari-oglindit.scss',
+})
+export class Class9PrelucrariOglindit {}
